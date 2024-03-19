b3b9524eec9603f1cb178c66e585f0f03fc7539c
Cuong-Phan <carmenphan17@gmail.com>
2024-03-19 19:46:08 +0700

    Update User model

--- src/models/shop.model.js
+++ src/models/shop.model.js
@@ -9,17 +9,31 @@ var userSchema = new mongoose.Schema({
         type:String,
         required:true,
         unique:true,
-        index:true,
+        index:true
     },
     email:{
         type:String,
         required:true,
-        unique:true,
+        unique:true
     },
     password:{
         type:String,
-        required:true,
+        required:true
     },
+    status:{
+        type:String,
+        enum:['active' , 'inactive'],
+        default: 'inactive'
+    },
+    verify:{
+        type: mongoose.Schema.Types.Boolean,
+        default:false
+    },
+    roles:{
+        type: Array,
+        default:[]
+    }
+
 }, {
     collection : COLLECTION_NAME,
     timestamps : true
